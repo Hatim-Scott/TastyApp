7c067ae04d33c11b48a4582e3cad18b304b8d1b9
Hatim-Scott <147860928+Hatim-Scott@users.noreply.github.com>
2025-07-30 16:34:18 +0100

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Mehdi Ammor on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
